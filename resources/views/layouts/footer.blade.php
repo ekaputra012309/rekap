3571fda0875c85624970adf34465f56ac5792528
ekaputra012309 <eko012309@gmail.com>
2025-06-26 16:38:17 +0700

    update data footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,13 @@
 <!-- Footer -->
 <footer class="footer bg-light py-3">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-6 text-start">
-        <p class="mb-25">&copy; 2025 RS Orthopedi Siaga Raya</p>
-      </div>
-      <div class="col-6 text-end">
-        <p class="mb-0">Version 1.0.0</p>
-      </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 text-start">
+                <p class="mb-25">&copy; 2025 Kitacodinginaja</p>
+            </div>
+            <div class="col-6 text-end">
+                <p class="mb-0">Version 1.0.0</p>
+            </div>
+        </div>
     </div>
-  </div>
 </footer>
